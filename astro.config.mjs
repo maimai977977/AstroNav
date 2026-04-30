e13ac595cd65b07032a69c7f8d6d70b5a5ac811d
maimai977977 <949022711@qq.com>
2026-04-30 20:20:34 +0800

    添加仓库路径

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://maimai977977.github.io',
+     base: '/AstorNav/',     // 注意开头的斜杠和末尾的斜杠
   vite: {
     plugins: [tailwindcss()]
   },
